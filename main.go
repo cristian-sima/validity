ebc0bb44f59f3dd4f4d2df6a367109afd98ecbe6
cristian-sima <cs25g13@soton.ac.uk>
2016-04-16 20:33:59 +0300

    Added special and float rules

---
+++ main.go
@@ -0,0 +1,32 @@
+package validity
+
+const (
+
+	// Float rules
+
+	// FloatValue -
+	FloatValue = "value"
+	// FloatStrictValue -
+	FloatStrictValue = "value_strict"
+	// FloatMax -
+	FloatMax = "max"
+	// FloatMin -
+	FloatMin = "min"
+	// FloatDigits -
+	FloatDigits = "digits"
+
+	// --- Special rules
+
+	// SpecialIBAN -
+	SpecialIBAN = "iban"
+	// SpecialCIF -
+	SpecialCIF = "cif"
+	// SpecialCnp -
+	SpecialCnp = "cnp"
+	// SpecialShortDate -
+	SpecialShortDate = "shortDate"
+	// SpecialLongDate -
+	SpecialLongDate = "longDate"
+	// SpecialEmail -
+	SpecialEmail = "email"
+)
